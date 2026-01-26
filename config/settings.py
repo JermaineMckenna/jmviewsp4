7364d950b7c2861a452a0e9ce60700eca6841957
JermaineMckenna <jermaine.mckenna_@outlook.com>
2026-01-26 08:15:09 +0000

    changed database for postgres on heroku

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,14 @@
 from pathlib import Path
 import os
+import dj_database_url  # type: ignore
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}", # type: ignore
+        conn_max_age=600,
+        ssl_require=not DEBUG, # type: ignore
+    )
+}
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,13 +63,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
-
-DATABASES = {
-	"default": {
-		"ENGINE": "django.db.backends.sqlite3",
-		"NAME": BASE_DIR / "db.sqlite3",
-	}
-}
 
 AUTH_PASSWORD_VALIDATORS = [
 	{"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
