922e01f30fd71e1c9bd705978eac06eb62637bfd
JermaineMckenna <jermaine.mckenna_@outlook.com>
2026-03-10 04:40:24 +0000

    changed debug to false/off to comply with assesor requirements

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-insecure-change-me")
 
-DEBUG = os.getenv("DEBUG", "True") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # --- Hosts / Domains ---
 # Keep env-driven, but ensure your custom domain + herokuapp are included by default
